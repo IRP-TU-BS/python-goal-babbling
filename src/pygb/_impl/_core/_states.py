44384c223106703f8f3f4a0b04d5fe192f1a91b0
qworkz11 <nico.weil@web.de>
2023-08-04 16:07:31 +0200

    Implement abstract state to provide proper type hints

--- src/pygb/_impl/_core/_states.py
+++ src/pygb/_impl/_core/_states.py
@@ -1,15 +1,22 @@
 from abc import ABC, abstractmethod
+from typing import Generic
 
-from pygb._impl._core._context import AbstractContext
+from pygb._impl._core._context import ContextType
 from pygb._impl._core._events import EventSystem
 
 
-class AbstractState(ABC):
-    def __init__(self, context: AbstractContext, event_system: EventSystem, name: str | None = None) -> None:
+class AbstractState(ABC, Generic[ContextType]):
+    def __init__(self, context: ContextType, event_system: EventSystem | None = None, name: str | None = None) -> None:
         self.name = name or self.__class__.__qualname__
         self.context = context
         self.events = event_system
 
     @abstractmethod
-    def __call__(self) -> None:
+    def __call__(self) -> str:
         ...
+
+    def __eq__(self, __o: object) -> bool:
+        if not isinstance(__o, type(self)):
+            return False
+
+        return self.name == __o.name
