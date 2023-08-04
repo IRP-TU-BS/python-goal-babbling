9b1a06f37bec9913fe4a13318206241c91202466
qworkz11 <nico.weil@web.de>
2023-08-04 16:07:31 +0200

    Add runtime data to GB context

--- src/pygb/_impl/_core/_context.py
+++ src/pygb/_impl/_core/_context.py
@@ -3,6 +3,7 @@ from typing import TypeVar
 
 from pygb._impl._core._goals import GoalStore
 from pygb._impl._core._parameters import GBParameterStore
+from pygb._impl._core._runtime_data import RuntimeData
 
 
 class AbstractContext(ABC):
@@ -15,21 +16,22 @@ class AbstractContext(ABC):
         Returns:
             Whether or not the state machine is running.
         """
-        ...
 
     @abstractmethod
     def set_running(self) -> None:
         """Setter for the state machine (which uses the context) is running."""
-        ...
 
 
 ContextType = TypeVar("ContextType", bound=AbstractContext)
 
 
 class GoalBabblingContext(AbstractContext):
-    def __init__(self, param_store: GBParameterStore, goal_store: GoalStore) -> None:
+    def __init__(
+        self, param_store: GBParameterStore, goal_store: GoalStore, runtime_data: RuntimeData = RuntimeData()
+    ) -> None:
         self.gb_param_store = param_store
         self.goal_store = goal_store
+        self.runtime_data = runtime_data
 
     def is_running(self) -> bool:
         # TODO
