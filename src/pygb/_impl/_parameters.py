686ceaee66e2c8aa73410ebe16c29c4d4edc7e46
qworkz11 <nico.weil@web.de>
2023-08-04 16:07:31 +0200

    Add parameter store

--- src/pygb/_impl/_parameters.py
+++ src/pygb/_impl/_parameters.py
@@ -15,6 +15,23 @@ class GBParameters:
     epoch_sets: int
     home_action_set: np.ndarray
 
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, type(self)):
+            return False
+
+        for attribute_name in self.__match_args__:
+            attribute = self.__getattribute__(attribute_name)
+            other_attribute = other.__getattribute__(attribute_name)
+
+            if isinstance(attribute, np.ndarray):
+                if not np.all(attribute == other_attribute):
+                    return False
+            else:
+                if attribute != other_attribute:
+                    return False
+
+        return True
+
     def combine(self, increment: "GBParameterIncrement") -> "GBParameters":
         combined = deepcopy(self)
         for attribute_name in increment.__match_args__:
@@ -34,3 +51,38 @@ class GBParameterIncrement:
     len_epoch: int | None = None
     epoch_sets: int | None = None
     home_action_set: np.ndarray | None = None
+
+
+class GBParameterStore:
+    def __init__(self, gb_parameters: GBParameters | list[GBParameters | GBParameterIncrement]) -> None:
+        if isinstance(gb_parameters, list):
+            self.gb_parameters = self._combine_parameter_sets(gb_parameters)
+        else:
+            self.gb_parameters = [gb_parameters]
+
+    def __getitem__(self, epoch_index: int) -> GBParameters:
+        return self.gb_parameters[epoch_index]
+
+    def _combine_parameter_sets(self, gb_parameters: list[GBParameters | GBParameterIncrement]) -> list[GBParameters]:
+        previous_default = gb_parameters[0]
+        if not isinstance(previous_default, GBParameters):
+            raise ValueError(
+                f"""Failed to extract GB parameter list: First entry must be of type {GBParameters.__name__} """
+                f"""but is of type {type(previous_default)}"""
+            )
+
+        params = [previous_default]
+        if len(gb_parameters) == 1:
+            return params
+
+        for index, p in enumerate(gb_parameters):
+            if index == 0:
+                continue
+
+            if isinstance(p, GBParameterIncrement):
+                params.append(previous_default.combine(p))
+            else:
+                params.append(p)
+                previous_default = p
+
+        return params
--- tests/unit/_impl/test_parameters.py
+++ tests/unit/_impl/test_parameters.py
@@ -4,7 +4,16 @@ import numpy as np
 import pytest
 from numpy.testing import assert_array_equal
 
-from pygb import GBParameterIncrement, GBParameters
+from pygb import GBParameterIncrement, GBParameters, GBParameterStore
+
+
+def test_gb_parameters_eq() -> None:
+    assert GBParameters(1.0, 2.0, 3, 4, 5, 6, 7, np.array([1.0])) == GBParameters(
+        1.0, 2.0, 3, 4, 5, 6, 7, np.array([1.0])
+    )
+    assert not GBParameters(1.0, 2.0, 3, 4, 5, 6, 7, np.array([1.0])) == GBParameters(
+        1.0, 42.0, 3, 4, 5, 6, 7, np.array([1.0])
+    )
 
 
 @pytest.mark.parametrize(
@@ -45,3 +54,47 @@ def test_combine_parameters_with_increment_home_action_sequence() -> None:
         assert combined.__getattribute__(attribute_name) is not None
 
     assert_array_equal(combined.home_action_set, increment.home_action_set)
+
+
+@pytest.mark.parametrize(
+    ("gb_parameters", "parsed_list"),
+    (
+        (
+            [
+                GBParameters(0.1, 0.01, 1, 2, 10, 20, 1, np.array([1.0])),
+                GBParameters(10.0, 20.0, 30, 20, 100, 200, 10, np.array([1.0])),
+            ],
+            [
+                GBParameters(0.1, 0.01, 1, 2, 10, 20, 1, np.array([1.0])),
+                GBParameters(10.0, 20.0, 30, 20, 100, 200, 10, np.array([1.0])),
+            ],
+        ),
+        (
+            [
+                GBParameters(0.1, 0.01, 1, 2, 10, 20, 1, np.array([1.0])),
+                GBParameters(10.0, 20.0, 30, 20, 100, 200, 10, np.array([1.0])),
+                GBParameterIncrement(sigma=42.0),
+            ],
+            [
+                GBParameters(0.1, 0.01, 1, 2, 10, 20, 1, np.array([1.0])),
+                GBParameters(10.0, 20.0, 30, 20, 100, 200, 10, np.array([1.0])),
+                GBParameters(42.0, 20.0, 30, 20, 100, 200, 10, np.array([1.0])),
+            ],
+        ),
+        (
+            GBParameters(0.1, 0.01, 1, 2, 10, 20, 1, np.array([1.0])),
+            [GBParameters(0.1, 0.01, 1, 2, 10, 20, 1, np.array([1.0]))],
+        ),
+    ),
+)
+def test_parameter_store_combine_parameter_sets(
+    gb_parameters: GBParameters | list[GBParameters | GBParameterIncrement], parsed_list: list[GBParameters]
+) -> None:
+    store = GBParameterStore(gb_parameters)
+
+    assert store.gb_parameters == parsed_list
+
+
+def test_parameter_store_raises_if_first_parameter_set_is_increment() -> None:
+    with pytest.raises(ValueError):
+        GBParameterStore([GBParameterIncrement(sigma=42.0)])
