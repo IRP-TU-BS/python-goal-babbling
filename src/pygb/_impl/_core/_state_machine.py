58a1282df63978b946674981fc6102ed5a2096ed
qworkz11 <nico.weil@web.de>
2023-08-04 16:07:31 +0200

    Add state machine and unit tests

--- src/pygb/_impl/_core/_state_machine.py
+++ src/pygb/_impl/_core/_state_machine.py
@@ -1,3 +1,5 @@
+"""Module which contains the state machine."""
+
 from typing import Any
 
 from pygb._impl._core._context import AbstractContext
@@ -5,7 +7,15 @@ from pygb._impl._core._states import AbstractState
 
 
 class StateMachine:
+    """State machine class."""
+
     def __init__(self, context: AbstractContext, initial_state: AbstractState | None = None) -> None:
+        """Constructor.
+
+        Args:
+            context: Context instance.
+            initial_state: Initial state. Defaults to None.
+        """
         self.context = context
 
         self._current_state = initial_state
@@ -14,18 +24,37 @@ class StateMachine:
 
     @property
     def current_state(self) -> AbstractState | None:
+        """Current state property. Is updated with each state machine cycle.
+
+        Returns:
+            The currently active state.
+        """
         return self._current_state
 
     @current_state.setter
     def current_state(self, _v: Any) -> None:
+        """Current state setter, which is prohibited."""
         raise NotImplementedError("Failed to set the current state: Setting the state is not allowed.")
 
     @property
     def initial_state(self) -> AbstractState | None:
+        """Initial state getter. The initial state remains the same once the state machine is started.
+
+        Returns:
+            The initial state.
+        """
         return self._initial_state
 
     @initial_state.setter
     def initial_state(self, state: AbstractState) -> None:
+        """Initial state setter. The initial state can only be set as long as the state machine is not running.
+
+        Args:
+            state: State instance.
+
+        Raises:
+            RuntimeError: If used while the state machine is running.
+        """
         if self.context.is_running():
             raise RuntimeError("Failed to set the initial state: State machine is already running.")
 
@@ -33,6 +62,7 @@ class StateMachine:
         self._initial_state = state
 
     def run(self) -> None:
+        """Starts the state machine. The state machine can be stopped by setting the running flag inside the context."""
         self.context.set_running()
 
         while self.context.is_running():
@@ -46,6 +76,16 @@ class StateMachine:
             self._current_state = self._transition_table[transition]
 
     def add(self, transition_name: str, state: AbstractState, no_raise: bool = False) -> None:
+        """Add a state transition.
+
+        Args:
+            transition_name: Name of the transition.
+            state: Target state. It is loaded as the next state if another state returns 'transition_name'.
+            no_raise: Disables raising an error if the transition name already exists. Defaults to False.
+
+        Raises:
+            ValueError: If the 'transition_name' already exists in the transition table and 'no_raise' is set to False.
+        """
         if transition_name in self._transition_table and not no_raise:
             raise ValueError(
                 f"""Failed to add state '{state.name}': Transition '{transition_name}' already has a target state """
@@ -56,9 +96,22 @@ class StateMachine:
         self._transition_table[transition_name] = state
 
     def pop(self, transition_name: str, no_raise: bool = True) -> AbstractState | None:
+        """Removes a transition from the transition table.
+
+        Args:
+            transition_name: The transition to remove
+            no_raise: Disables raising an error if 'tranisition_name' is unknown. Defaults to True.
+
+        Raises:
+            KeyError: If 'transition_name' is unknown and 'no_raise' is set to False.
+
+        Returns:
+            The target state of the removed transition or None if the transition is unknown.
+        """
         if transition_name in self._transition_table:
             return self._transition_table.pop(transition_name)
-        elif not no_raise:
+
+        if no_raise:
             return None
 
         raise KeyError(
--- tests/unit/_impl/_core/test_state_machine.py
+++ tests/unit/_impl/_core/test_state_machine.py
@@ -1,3 +1,5 @@
+import pytest
+
 from pygb import AbstractContext, AbstractState, EventSystem, StateMachine
 
 
@@ -6,7 +8,7 @@ class DummyContext(AbstractContext):
         super().__init__()
 
         self.running = False
-        self.counter = 0
+        self.state_calls: list[str] = []
 
     def is_running(self) -> bool:
         return self.running
@@ -15,9 +17,104 @@ class DummyContext(AbstractContext):
         self.running = True
 
 
-class DummyState(AbstractState):
-    def __init__(self, context: AbstractContext, event_system: EventSystem, name: str | None = None) -> None:
+class DummyState(AbstractState[DummyContext]):
+    def __init__(self, context: DummyContext, event_system: EventSystem | None = None, name: str | None = None) -> None:
         super().__init__(context, event_system, name)
 
-    def __call__(self) -> None:
-        self.context.co
+    def __call__(self) -> str:
+        self.context.state_calls.append(self.name)
+        if len(self.context.state_calls) >= 3:
+            self.context.running = False
+
+        return self.name
+
+
+def test_init() -> None:
+    context = DummyContext()
+    state = DummyState(context)
+    sm = StateMachine(context, initial_state=state)
+
+    assert sm._transition_table == {}
+
+
+def test_set_initial_state() -> None:
+    context = DummyContext()
+    state = DummyState(context, name="dummy_state")
+
+    sm = StateMachine(context)
+    sm.context.running = True
+
+    with pytest.raises(RuntimeError):
+        sm.initial_state = state
+
+    sm.context.running = False
+    sm.initial_state = state
+
+    assert sm.initial_state == state
+    assert sm.current_state == state
+
+
+def test_set_current_state() -> None:
+    context = DummyContext()
+    state = DummyState(context, name="dummy_state")
+    sm = StateMachine(context)
+
+    with pytest.raises(NotImplementedError):
+        sm.current_state = state
+
+    sm.initial_state = state
+    assert sm.current_state == state
+
+
+def test_add() -> None:
+    context = DummyContext()
+    state = DummyState(context, name="dummy_state")
+    sm = StateMachine(context)
+
+    sm.add("test_transition", state)
+
+    assert sm._transition_table == {"test_transition": state}
+
+    with pytest.raises(ValueError):
+        sm.add("test_transition", state)
+
+    state2 = DummyState(context, name="dummy_state2")
+    sm.add("test_transition", state2, no_raise=True)
+
+    assert sm._transition_table == {"test_transition": state2}
+
+
+def test_pop() -> None:
+    context = DummyContext()
+    sm = StateMachine(context)
+    state1 = DummyState(context, name="state1")
+    state2 = DummyState(context, name="state2")
+
+    sm._transition_table = {"transition1": state1, "transition2": state2}
+
+    with pytest.raises(KeyError):
+        sm.pop("unknown_transition", no_raise=False)
+
+    removed_state = sm.pop("unknown_transition", no_raise=True)
+    assert removed_state is None
+
+    removed_state = sm.pop("transition2")
+    assert removed_state == state2
+    assert sm._transition_table == {"transition1": state1}
+
+
+def test_run() -> None:
+    context = DummyContext()
+    state1 = DummyState(context, name="state1")
+    state2 = DummyState(context, name="state2")
+    sm = StateMachine(context, initial_state=state1)
+
+    sm.add(state1.name, state2)
+    sm.add(state2.name, state1)
+
+    assert sm._transition_table == {"state1": state2, "state2": state1}
+
+    sm.run()
+
+    assert not sm.context.is_running()
+    assert context.state_calls == ["state1", "state2", "state1"]
