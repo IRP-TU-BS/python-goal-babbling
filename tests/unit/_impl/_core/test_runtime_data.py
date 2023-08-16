465770a507112ee4e043500f034f06bde7e18407
qworkz11 <nico.weil@web.de>
2023-08-16 08:10:23 +0200

    Remove update_current_sequence form RuntimeData

--- tests/unit/_impl/_core/test_runtime_data.py
+++ tests/unit/_impl/_core/test_runtime_data.py
@@ -1,4 +1,4 @@
-from pygb import RuntimeData, ObservationSequence
+from pygb import ObservationSequence, RuntimeData
 
 
 def test_runtime_data_init() -> None:
@@ -9,21 +9,3 @@ def test_runtime_data_init() -> None:
     assert runtime_data.performance_error is None
     assert len(runtime_data.sequences) == 0
     assert runtime_data.observation_index == 0
-
-
-def test_runtime_data_update_current_sequence() -> None:
-    runtime_data = RuntimeData()
-    sequence = ObservationSequence(0, 1)
-
-    runtime_data.update_current_sequence(sequence)
-
-    assert runtime_data.previous_sequence is None
-    assert runtime_data.current_sequence == sequence
-    assert runtime_data.sequences == []
-
-    sequence2 = ObservationSequence(1, 3)
-    runtime_data.update_current_sequence(sequence2)
-
-    assert runtime_data.previous_sequence == sequence
-    assert runtime_data.current_sequence == sequence2
-    assert runtime_data.sequences == []
