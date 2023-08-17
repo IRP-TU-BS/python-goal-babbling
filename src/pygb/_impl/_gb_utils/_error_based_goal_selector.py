ca4334137241b664cddb12854dc206e1fd529300
qworkz11 <nico.weil@web.de>
2023-08-17 16:36:14 +0200

    Move goal selectors to separate modules

--- src/pygb/_impl/_gb_utils/_error_based_goal_selector.py
+++ src/pygb/_impl/_gb_utils/_error_based_goal_selector.py
@@ -5,44 +5,6 @@ from pygb._impl._core._context import GoalBabblingContext
 from pygb._impl._core._runtime_data import ActionSequence, ObservationSequence
 
 
-class RandomGoalSelector(AbstractGoalSelector[GoalBabblingContext]):
-    """Random goal selector class."""
-
-    def __init__(self, random_seed: int | None = None) -> None:
-        """Constructor.
-
-        Args:
-            random_seed: Random seed which is used to initilaize a numpy random number generator. Defaults to None.
-        """
-        self._rng = np.random.default_rng(seed=random_seed)
-
-    def select(self, context: GoalBabblingContext) -> tuple[int, np.ndarray]:
-        """Selects one goal randomly that is different from the previous sequence's stop goal.
-
-        Args:
-            context: Goal Babbling context.
-
-        Returns:
-            Randomly selected goal index and the goal itself.
-        """
-        if context.runtime_data.previous_sequence is None or isinstance(
-            context.runtime_data.previous_sequence, ActionSequence
-        ):
-            # start of epoch set or we've just been at home -> choose any goal randomly
-            random_idx = self._rng.integers(0, context.current_goal_set.train.shape[0], size=None)
-            selected_goal = context.current_goal_set.train[random_idx]
-
-        elif isinstance(context.runtime_data.previous_sequence, ObservationSequence):
-            previous_goal = context.runtime_data.previous_sequence.stop_goal
-            selected_goal = None
-
-            while selected_goal is None or np.all(previous_goal == selected_goal):
-                random_idx = self._rng.integers(0, context.current_goal_set.train.shape[0], size=None)
-                selected_goal = context.current_goal_set.train[random_idx]
-
-        return random_idx, selected_goal
-
-
 class ErrorBasedGoalSelector(AbstractGoalSelector[GoalBabblingContext]):
     """Goal selector class which selects a goal randomly from the top X% training goals with highest prediction
     error."""
---
+++ tests/unit/_impl/_gb_utils/test_random_goal_selector.py
@@ -0,0 +1,74 @@
+from unittest.mock import MagicMock, call
+
+import numpy as np
+import pytest
+from numpy.testing import assert_array_equal
+
+from pygb import (
+    ActionSequence,
+    GoalBabblingContext,
+    GoalSet,
+    GoalStore,
+    ObservationSequence,
+    RandomGoalSelector,
+    RuntimeData,
+    SequenceType,
+)
+
+
+def create_context_mock(previous_sequence: SequenceType | None = None) -> GoalBabblingContext:
+    goal_store = GoalStore(
+        [GoalSet(np.array([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]]), np.array([[10.0], [20.0], [30.0]]))]
+    )
+    context = GoalBabblingContext(
+        param_store=None,
+        goal_store=goal_store,
+        forward_model=None,
+        inverse_estimate=None,
+        runtime_data=RuntimeData(
+            sequences=[ObservationSequence(start_goal=np.array([1.0, 2.0]), stop_goal=np.array([3.0, 4.0]))],
+            previous_sequence=previous_sequence,
+        ),
+    )
+
+    return context
+
+
+@pytest.mark.parametrize(
+    ("previous_sequence", "expected_index", "expected_goal"),
+    [
+        (ObservationSequence(start_goal=np.array([3.0, 4.0]), stop_goal=np.array([5.0, 6.0])), 1, np.array([3.0, 4.0])),
+        (ActionSequence(start_action=None, stop_action=None), 1, np.array([3.0, 4.0])),
+        (None, 1, np.array([3.0, 4.0])),
+    ],
+)
+def test_random_goal_selector(previous_sequence: SequenceType, expected_index: int, expected_goal: np.ndarray) -> None:
+    selector = RandomGoalSelector()
+
+    rng_mock = MagicMock()
+    rng_mock.integers = MagicMock(return_value=1)
+    selector._rng = rng_mock
+    context = create_context_mock(previous_sequence)
+
+    index, goal = selector.select(context)
+
+    selector._rng.integers.assert_called_once_with(0, 3, size=None)
+    assert index == expected_index
+    assert_array_equal(goal, expected_goal)
+
+
+def test_random_goal_selector_does_not_choose_previous_stop_goal() -> None:
+    selector = RandomGoalSelector()
+
+    # create RNG which returns previously visited goal index (2), then one which has not been previously visited (0)
+    rng_mock = MagicMock()
+    rng_mock.integers = MagicMock()
+    rng_mock.integers.side_effect = [2, 0]
+    selector._rng = rng_mock
+    context = create_context_mock(ObservationSequence(start_goal=np.array([3.0, 4.0]), stop_goal=np.array([5.0, 6.0])))
+
+    index, goal = selector.select(context)
+
+    selector._rng.integers.assert_has_calls([call(0, 3, size=None), call(0, 3, size=None)])
+    assert index == 0
+    assert_array_equal(goal, np.array([1.0, 2.0]))
