8a82d057249c67c9818021d2e6a449c95066072e
qworkz11 <nico.weil@web.de>
2023-08-31 15:07:47 +0200

    Add start epoch to stopping criteria

--- src/pygb/_impl/_core/_stopping_criteria.py
+++ src/pygb/_impl/_core/_stopping_criteria.py
@@ -10,13 +10,15 @@ from pygb._impl._core._context import GoalBabblingContext
 class TargetPerformanceStop(AbstractStoppingCriteria[GoalBabblingContext]):
     """Stopping cirterion which stops the training as soon as a target performance error is reached."""
 
-    def __init__(self, performance: float) -> None:
+    def __init__(self, performance: float, start_epoch: int = 0) -> None:
         """Constructor.
 
         Args:
             performance: Target performance (e.g. RMSE).
+            start_epoch: Epoch at which this stopping criterion becomes active (starting at 0). Defaults to 0.
         """
         self.performance = performance
+        self.start_epoch = start_epoch
 
     def fulfilled(self, context: GoalBabblingContext) -> bool:
         """Checks if the target performance error is reached.
@@ -27,23 +29,28 @@ class TargetPerformanceStop(AbstractStoppingCriteria[GoalBabblingContext]):
         Returns:
             True if the target performance error is reached, False otherwise.
         """
-        if context.runtime_data.performance_error <= self.performance:
-            return True
+        if (
+            context.runtime_data.performance_error > self.performance
+            or context.runtime_data.epoch_index < self.start_epoch
+        ):
+            return False
 
-        return False
+        return True
 
 
 @dataclass
 class TimeBudgetStop(AbstractStoppingCriteria[GoalBabblingContext]):
     """Stopping criterion which stops training after a specified time budget is reached."""
 
-    def __init__(self, budget: timedelta) -> None:
+    def __init__(self, budget: timedelta, start_epoch: int = 0) -> None:
         """Constructor.
 
         Args:
             budget: Time budget.
+            start_epoch: Epoch at which this stopping criterion becomes active (starting at 0). Defaults to 0.
         """
         self.budget = budget
+        self.start_epoch = start_epoch
 
         self._start: datetime | None = None
 
@@ -62,6 +69,9 @@ class TimeBudgetStop(AbstractStoppingCriteria[GoalBabblingContext]):
             self._start = datetime.now()
             return False
 
+        if context.runtime_data.epoch_index < self.start_epoch:
+            return False
+
         return datetime.now() - self._start >= self.budget
 
 
@@ -69,13 +79,15 @@ class TimeBudgetStop(AbstractStoppingCriteria[GoalBabblingContext]):
 class PerformanceSlopeStop(AbstractStoppingCriteria[GoalBabblingContext]):
     """Stopping criterion which stops training if the performance error increases over a specified amount of epochs."""
 
-    def __init__(self, grace_period: int) -> None:
+    def __init__(self, grace_period: int, start_epoch: int = 0) -> None:
         """Constructor.
 
         Args:
             grace_period: Amount of epochs in which a performance error increase is allowed.
+            start_epoch: Epoch at which this stopping criterion becomes active (starting at 0). Defaults to 0.
         """
         self.grace_period = grace_period
+        self.start_epoch = start_epoch
 
         self._best_performance: float | None = None
         self._period = 0
@@ -92,6 +104,9 @@ class PerformanceSlopeStop(AbstractStoppingCriteria[GoalBabblingContext]):
         Returns:
             True if the performance has not been increasing for longer than the specified grace period, False otherwise.
         """
+        if context.runtime_data.epoch_index < self.start_epoch:
+            return False
+
         if self._best_performance is None:
             self._best_performance = context.runtime_data.performance_error
             return False
--- tests/unit/_impl/_core/test_stopping_criteria.py
+++ tests/unit/_impl/_core/test_stopping_criteria.py
@@ -12,31 +12,35 @@ from pygb import (
 )
 
 
-@pytest.mark.parametrize(("performance_error", "output"), [(42.1, False), (42.0, True), (41.9, True)])
-def test_target_performance_stop(performance_error: float, output: bool) -> None:
+@pytest.mark.parametrize(
+    ("performance_error", "epoch", "output"), [(42.1, 6, False), (42.0, 6, True), (41.9, 6, True), (41.0, 4, False)]
+)
+def test_target_performance_stop(performance_error: float, epoch: int, output: bool) -> None:
     context_mock = MagicMock(
-        spec=GoalBabblingContext, runtime_data=MagicMock(spec=RuntimeData, performance_error=performance_error)
+        spec=GoalBabblingContext,
+        runtime_data=MagicMock(spec=RuntimeData, performance_error=performance_error, epoch_index=epoch),
     )
 
-    criterion = TargetPerformanceStop(performance=42.0)
+    criterion = TargetPerformanceStop(performance=42.0, start_epoch=5)
 
     assert criterion.fulfilled(context_mock) == output
 
 
 @pytest.mark.parametrize(
-    ("now_side_effect", "budget", "output"),
+    ("now_side_effect", "budget", "epoch", "output"),
     [
-        ([datetime(2023, 8, 1, 10, 0, 0), datetime(2023, 8, 1, 10, 9, 59)], timedelta(minutes=10), False),
-        ([datetime(2023, 8, 1, 10, 0, 0), datetime(2023, 8, 1, 10, 10, 0)], timedelta(minutes=10), True),
+        ([datetime(2023, 8, 1, 10, 0, 0), datetime(2023, 8, 1, 10, 9, 59)], timedelta(minutes=10), 5, False),
+        ([datetime(2023, 8, 1, 10, 0, 0), datetime(2023, 8, 1, 10, 10, 0)], timedelta(minutes=10), 5, True),
+        ([datetime(2023, 8, 1, 10, 0, 0), datetime(2023, 8, 1, 10, 10, 0)], timedelta(minutes=10), 4, False),
     ],
 )
-def test_time_budget_stop(now_side_effect: list[datetime], budget: timedelta, output: bool) -> None:
-    context_mock = MagicMock(spec=GoalBabblingContext)
+def test_time_budget_stop(now_side_effect: list[datetime], budget: timedelta, epoch: int, output: bool) -> None:
+    context_mock = MagicMock(spec=GoalBabblingContext, runtime_data=MagicMock(spec=RuntimeData, epoch_index=epoch))
 
     with patch("pygb._impl._core._stopping_criteria.datetime", wraps=datetime) as now_mock:
         now_mock.now.side_effect = now_side_effect
 
-        criterion = TimeBudgetStop(budget=budget)
+        criterion = TimeBudgetStop(budget=budget, start_epoch=5)
 
         # first call set _start attribute:
         criterion.fulfilled(context_mock) == False
@@ -45,7 +49,9 @@ def test_time_budget_stop(now_side_effect: list[datetime], budget: timedelta, ou
 
 
 def test_performance_slope_stop_sets_new_best_performance() -> None:
-    context_mock = MagicMock(spec=GoalBabblingContext, runtime_data=MagicMock(spec=RuntimeData, performance_error=24.0))
+    context_mock = MagicMock(
+        spec=GoalBabblingContext, runtime_data=MagicMock(spec=RuntimeData, performance_error=24.0, epoch_index=4)
+    )
 
     criterion = PerformanceSlopeStop(grace_period=10)
     criterion._best_performance = 42.0
@@ -57,8 +63,22 @@ def test_performance_slope_stop_sets_new_best_performance() -> None:
     assert criterion._best_performance == 24.0
 
 
+def test_performance_slope_returns_false_if_start_epoch_not_reached() -> None:
+    context_mock = MagicMock(
+        spec=GoalBabblingContext, runtime_data=MagicMock(spec=RuntimeData, performance_error=24.0, epoch_index=4)
+    )
+
+    criterion = PerformanceSlopeStop(grace_period=10, start_epoch=5)
+    criterion._best_performance = 43.0
+    criterion._period = 11
+
+    assert not criterion.fulfilled(context_mock)
+
+
 def test_performance_slope_stop_updates_period_and_stops_training() -> None:
-    context_mock = MagicMock(spec=GoalBabblingContext, runtime_data=MagicMock(spec=RuntimeData, performance_error=44.0))
+    context_mock = MagicMock(
+        spec=GoalBabblingContext, runtime_data=MagicMock(spec=RuntimeData, performance_error=44.0, epoch_index=4)
+    )
 
     criterion = PerformanceSlopeStop(grace_period=10)
     criterion._best_performance = 42.0
