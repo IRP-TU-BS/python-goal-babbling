12afc15777aa84772e27640aaddaf35aae76adde
qworkz11 <nico.weil@web.de>
2023-09-07 21:19:13 +0200

    Remove test module

--- tests/unit/test_test_module.py
+++
@@ -1,8 +0,0 @@
-from pytest import CaptureFixture
-
-from pygb.test_module import print_stuff
-
-
-def test_print_stuff(capsys: CaptureFixture) -> None:
-    print_stuff()
-    assert capsys.readouterr().out == "stuff\n"
